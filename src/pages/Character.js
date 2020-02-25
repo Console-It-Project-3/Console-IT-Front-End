827c7ea1205dc4abd16124ee3570c88cc126ac1b
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-25 14:30:27 -0600

    finished Character wireframe

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,9 +1,21 @@
-import React, {Component} from "react";
-
-class Character extends Component{
-render(){
-    return <h1>Hello Darkness my old friend</h1>
-}
+import React, { Component } from "react";
+import Sprite from "../components/Character"
+import Scroll from "../components/scroll"
+class Character extends Component {
+    render() {
+        return (
+            <div className="charContain">
+                <div className="row">
+                    <div className="col-md-6">
+                        <Sprite />
+                    </div>
+                    <div className="col-md-6">
+                        <Scroll />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Character
