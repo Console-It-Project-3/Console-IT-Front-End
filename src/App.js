ec0923f1c3fdbb0dbba051c7f66d575a1a090c7e
LPMartinez <lpmartinez1109@gmail.com>
2020-02-25 11:27:54 -0600

    Fixes made to app.js and index.js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Character from "./pages/Character";
+import Movement from "./pages/Movement";
+import Battle from "./pages/Battle";
+import Highscore from "./pages/Highscore";
+import NoMatch from "./pages/NoMatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Switch>
+          <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/character" component={Character} />
+            <Route exact path="/movement" component={Movement} />
+            <Route exact path="/battle" component={Battle} />
+            <Route exact path="/highscore" component={Highscore} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+
   );
 }
 
