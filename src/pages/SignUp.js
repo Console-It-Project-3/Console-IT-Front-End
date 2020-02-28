1f4030df1be6840d2ef76e0b8ac06e7758ab47d6
LPMartinez <lpmartinez1109@gmail.com>
2020-02-28 10:44:28 -0600

    Edits made to fix display problems on home page and sign up. Sign up box now allows first and last name

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,77 +1,64 @@
 import React, { Component } from "react";
 import "./SignUp.css"
+
+
+
 class Signup extends Component {
     render() {
         return (
-
-            <div className="signUp">
-
-          
-
-           
-
+            <div className="signup-box">
                 <div className="container-signup">
-
-                    <form class="needs-validation" novalidate>
-                        <div class="form-row">
-                            <div class="col-md-4 mb-3">
-                                <label for="validationCustom01">First name</label>
-                                <input type="text" class="form-control" value="Mark" required />
-                                <div class="valid-feedback">
+                    <form className="needs-validation" novalidate>
+                        <div className="form-row">
+                            <div className="col-md-4 mb-3">
+                                <label for="validationCustom01">First name: </label>
+                                <input type="text" className="form-control" placeholder="Enter your first name" required />
+                                <div className="valid-feedback">
                                     Looks good!
-      </div>
+                                </div>
                             </div>
-                            <div class="col-md-4 mb-3">
-                                <label for="validationCustom02">Last name</label>
-                                <input type="text" class="form-control" value="Otto" required />
-                                <div class="valid-feedback">
+                            <div className="col-md-4 mb-3">
+                                <label for="validationCustom02">Last name: </label>
+                                <input type="text" className="form-control" placeholder="Enter your last name" required />
+                                <div className="valid-feedback">
                                     Looks good!
-      </div>
+                                </div>
                             </div>
-                            <div class="col-md-4 mb-3">
-                                <label for="validationCustomUsername">Username</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text" i>@</span>
-                                    </div>
-                                    <input type="text" class="form-control" aria-describedby="inputGroupPrepend" required />
-                                    <div class="invalid-feedback">
+                            <div className="col-md-4 mb-3">
+                                <label for="validationCustomUsername">Username: </label>
+                                <div className="input-group">
+                                    
+                                    <input type="text" className="form-control" aria-describedby="inputGroupPrepend" required />
+                                    <div className="invalid-feedback">
                                         Please choose a username.
-        </div>
+                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                            <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputEmail3" />
+                        <div className="form-group row">
+                            <label for="inputEmail3" className="col-sm-2 col-form-label">Email: </label>
+                            <div className="col-sm-10">
+                                <input type="email" className="form-control" id="inputEmail3" />
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" id="inputPassword3" />
-                            </div>
-                            <div class="col-md-3 mb-3">
-                                <label for="validationCustom05">Zip</label>
-                                <input type="text" class="form-control" required />
-                                <div class="invalid-feedback">
-                                    Please provide a valid zip.
-      </div>
+                        <div className="form-group row">
+                            <label for="inputPassword3" className="col-sm-2 col-form-label">Password: </label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control" id="inputPassword3" />
                             </div>
                         </div>
-                        <div class="form-group">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" required />
-                                <label class="form-check-label" for="invalidCheck">
+                        <div className="form-group">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" required />
+                                <label className="form-check-label" for="invalidCheck">
                                     Agree to terms and conditions
-      </label>
-                                <div class="invalid-feedback">
+                                </label>
+                                <div className="invalid-feedback">
                                     You must agree before submitting.
-      </div>
+                                 </div>
                             </div>
                         </div>
-                        <button class="btn btn-primary" type="submit">Submit form</button>
+                        <button className="btn btn-primary" type="submit">Sign Up</button>
                     </form>
                 </div>
             </div>
