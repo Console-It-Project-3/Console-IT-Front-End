993ed40da352204e8299efae733073cbb415dcc2
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-28 11:39:28 -0600

    MVP battlepage

--- src/components/BattleStats/index.js
+++ src/components/BattleStats/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./BattleStats.css"
 
-function BattleStats({ heroHP, enemyHP }) {
+function BattleStats({ heroHP, enemyHP, heroMaxHP, enemyMaxHP }) {
     return (
         <>
             <div className="heroBox">
-                <p>HeroName</p>
-                <progress className="heroBar" id="file" value={heroHP} max="100"></progress>
+                <p>EnemyName</p>
+                <progress className="heroBar" id="file" value={heroHP} max={heroMaxHP}></progress>
+            </div>
+            <div className="heroImg">
             </div>
             <div className="enemyBox">
-                <p>EnemyName</p>
-                <progress className="enemyBar" id="file" value={enemyHP} max="50"></progress>
+                <p>HeroName</p>
+                <progress className="enemyBar" id="file" value={enemyHP} max={enemyMaxHP} ></progress>
+            </div>
+            <div className="enemyImg">
             </div>
         </>
     )
